20b048eb366c5d194e7712c14e7b7bec538aec6b
Sarkissian-Telmane <telmane.sarkissian@edu.univ-fcomte.fr>
2026-03-10 08:36:14 +0100

    fichiers backend + test-back enfin complétés

---
+++ backend/backend.mjs
@@ -0,0 +1,80 @@
+import PocketBase from 'pocketbase';
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+export async function artistesSorted() {
+    const records = await pb.collection('artistes').getFullList({ sort: 'date_representation' });
+    return records;
+}
+
+export async function scenesName() {
+    const records = await pb.collection('scenes').getFullList({ sort: 'nom_scene' });
+    return records;
+}
+
+export async function artistesName() {
+    const records = await pb.collection('artistes').getFullList({ sort: 'nom_artiste' });
+    return records;
+}
+
+export async function artisteID(id) {
+    const record = await pb.collection('artistes').getOne(id);
+    return record;
+}
+
+export async function sceneID(id) {
+    const record = await pb.collection('scenes').getOne(id);
+    return record;
+}
+
+export async function allartistebysceneId(id) {
+    const records = await pb.collection('artistes').getFullList({ filter: scenes = "${id}", sort: 'date_representation' });
+    return records;
+}
+
+export async function allartistebysceneName(nom) {
+    const scene = await pb.collection('scenes').getFirstListItem(nom = "${nom}");
+    const records = await pb.collection('artistes').getFullList({ filter: scene = "${scenes.id}", sort: 'date_representation' });
+    return records;
+}
+export async function addArtiste(artisteData) {
+    try {
+        const record = await pb.collection('artistes').create(artisteData);
+        console.log('Artiste ajouté :', record);
+        return record;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+export async function addScene(sceneData) {
+    try {
+        const record = await pb.collection('scenes').create(sceneData);
+        console.log('Scène ajoutée :', record);
+        return record;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+export async function updateArtiste(id, artisteData) {
+    try {
+        const record = await pb.collection('artistes').update(id, artisteData);
+        console.log('Artiste modifié :', record);
+        return record;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+export async function updateScene(id, sceneData) {
+    try {
+        const record = await pb.collection('scenes').update(id, sceneData);
+        console.log('Scène modifiée :', record);
+        return record;
+    } catch (error) {(error);
+        throw error;
+    }
+}
---
+++ backend/test-back.js
@@ -0,0 +1,110 @@
+import { artistesSorted, scenesName, artistesName, artisteID, sceneID, allartistebysceneId, allartistebysceneName, addArtiste, addScene, updateArtiste, updateScene } from './backend.mjs';
+
+
+
+// try {
+//     const records = await artistesSorted();
+//     console.log(JSON.stringify(records, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
+
+
+// try {
+//     const records = await scenesName();
+//     console.log(JSON.stringify(records, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
+
+
+// try {
+//     const records = await artistesName();
+//     console.log(JSON.stringify(records, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
+
+
+// try {
+//     const records = await artisteID('m57hylh0mkpa9vf');
+//     console.log(JSON.stringify(records, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
+
+
+// try {
+//     const records = await sceneID('h9q87py9tn1a316');
+//     console.log(JSON.stringify(records, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
+
+
+// try {
+//     const records = await allartistebysceneId('h9q87py9tn1a316');
+//     console.log(JSON.stringify(records, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
+
+
+// try {
+//     const records = await allartistebysceneName('Mureille');
+//     console.log(JSON.stringify(records, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
+
+
+// try {
+//     const artisteData = {
+//         "nom": "Mozart",
+//         "date_de_representation": "2026-08-30T23:45:00.000Z",
+//         "scene" : "h9q87py9tn1a316",
+//         "description" : "Lui-même réincarné",
+// };
+//     await addArtiste(artisteData);
+// } catch (e) {
+//     console.error(e);
+// }
+
+
+// try {
+//     const sceneData = {
+//         "nom": "Salle de bal",
+//         "artistes": ["6tppmyxqlignecd", "zeg78qfhga2umq5"],
+//         "description" : "La Salle de bal de la Citadelle",
+// };
+//     await addScene(sceneData);
+// } catch (e) {
+//     console.error(e);
+// }
+
+
+// try {
+//     const data = {
+//         "nom": "Beethoven",
+//         "date_de_representation": "2026-08-29T19:00:00.000Z",
+//         "scene" : "eagd112hd7dopre",
+//         "description" : "Lui-même réincarné en chair et en os",
+//     };
+//     const record = await updateArtiste('agld9fq1vpz8hck', data);
+//     console.log(JSON.stringify(record, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
+
+
+// try {
+//     const data = {
+//         "nom": "Cours intérieur",
+//         "artistes": ["m57hylh0mkpa9vf", "0tpgo2scsqgvbea"],
+//         "description": "Cette fois si, c'est la cours intérieur",
+//     };
+//     const record = await updateScene('eagd112hd7dopre', data);
+//     console.log(JSON.stringify(record, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
